fff1af84b578f54d591fac59332c89242521aa5e
Hannah Le <hannah.lgbhan@gmail.com>
2021-08-03 10:25:16 -0400

    Fixed inconsistent font size/ type in different browsers

--- react/src/components/Input.tsx
+++ react/src/components/Input.tsx
@@ -18,6 +18,8 @@ const Component = styled.input<InputProps>`
     border: ${props => props.theme.borders.thin};
     box-shadow: ${props => props.theme.boxShadow};
     padding: ${props => props.theme.space[0]} ${props => props.theme.space[4]};
+    font-family: ${props => props.theme.fontFamily.body};
+    font-size: ${props => props.theme.fontSizes.s};
     min-height: 46px;
 `;
 
