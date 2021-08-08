a416dfbd794709087f608ff0732263ee1b95a0c8
conor <conorklamann@gmail.com>
2021-08-08 18:05:33 -0400

    add update token on refresh

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
 import ReactDOM from 'react-dom';
+import { buildLink, client } from './apollo/client';
 import keycloak from './keycloak';
 import App from './pages/App';
 import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <ReactKeycloakProvider authClient={keycloak}>
+        <ReactKeycloakProvider
+            onTokens={tokens => client.setLink(buildLink(tokens.token))}
+            authClient={keycloak}
+        >
             <App />
         </ReactKeycloakProvider>
     </React.StrictMode>,
