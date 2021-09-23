d74ee916c041cadded339a0b1420532ad5c1de60
Hannah Le <hannah.lgbhan@gmail.com>
2021-09-23 17:53:08 -0400

    Removed Core from visible columns modal

--- react/src/components/Table/Table.tsx
+++ react/src/components/Table/Table.tsx
@@ -466,42 +466,44 @@ const Table: React.FC<TableProps> = ({ variantData }) => {
                         hideModal={() => setShowModal(false)}
                         title="Customize Columns"
                     >
-                        {headerGroups[0].headers.map((g, id) => (
-                            <div key={id}>
-                                <Checkbox
-                                    label={g.Header as string}
-                                    checked={g.isVisible}
-                                    onClick={() => handleGroupChange(g)}
-                                />
-                                {g.columns?.map(
-                                    (c, id) =>
-                                        !fixedColumns.includes(c.id) &&
-                                        !dummyColumns.includes(c.id) && (
-                                            <div key={id} style={{ paddingLeft: 20 }}>
-                                                <Checkbox
-                                                    label={c.Header as string}
-                                                    checked={c.isVisible}
-                                                    onClick={() => {
-                                                        if (
-                                                            c.parent &&
-                                                            g.columns?.filter(c => c.isVisible)
-                                                                .length === 1
-                                                        ) {
-                                                            toggleHideColumn(c.id, c.isVisible);
-                                                            toggleHideColumn(
-                                                                'empty_' + c.parent.id,
-                                                                !c.isVisible
-                                                            );
-                                                        } else {
-                                                            toggleHideColumn(c.id, c.isVisible);
-                                                        }
-                                                    }}
-                                                />
-                                            </div>
-                                        )
-                                )}
-                            </div>
-                        ))}
+                        {headerGroups[0].headers
+                            .filter(header => header.Header !== 'Core')
+                            .map((g, id) => (
+                                <div key={id}>
+                                    <Checkbox
+                                        label={g.Header as string}
+                                        checked={g.isVisible}
+                                        onClick={() => handleGroupChange(g)}
+                                    />
+                                    {g.columns?.map(
+                                        (c, id) =>
+                                            !fixedColumns.includes(c.id) &&
+                                            !dummyColumns.includes(c.id) && (
+                                                <div key={id} style={{ paddingLeft: 20 }}>
+                                                    <Checkbox
+                                                        label={c.Header as string}
+                                                        checked={c.isVisible}
+                                                        onClick={() => {
+                                                            if (
+                                                                c.parent &&
+                                                                g.columns?.filter(c => c.isVisible)
+                                                                    .length === 1
+                                                            ) {
+                                                                toggleHideColumn(c.id, c.isVisible);
+                                                                toggleHideColumn(
+                                                                    'empty_' + c.parent.id,
+                                                                    !c.isVisible
+                                                                );
+                                                            } else {
+                                                                toggleHideColumn(c.id, c.isVisible);
+                                                            }
+                                                        }}
+                                                    />
+                                                </div>
+                                            )
+                                    )}
+                                </div>
+                            ))}
                     </Modal>
                 </InlineFlex>
             </TableFilters>
