8f5fd5701c1da9c8255a39988d5704075d748f71
Hannah Le <hannah.lgbhan@gmail.com>
2021-09-30 10:10:11 -0400

    Delete prepare-minio.sh

--- prepare-minio.sh
+++
@@ -1,4 +0,0 @@
-mc alias set "${MINIO_HOST_ALIAS}" "${MINIO_HOST}" "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}"
-mc mb "${MINIO_HOST_ALIAS}/${MINIO_BUCKET}"
-# Set anonymous read and download access
-mc policy set download "${MINIO_HOST_ALIAS}/${MINIO_BUCKET}" 
