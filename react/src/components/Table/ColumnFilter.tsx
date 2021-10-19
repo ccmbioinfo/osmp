45e1fda92f9bda3398e79fdae90c32f13e0e9457
Hannah Le <hannah.lgbhan@gmail.com>
2021-10-19 16:56:16 -0400

    Removed logs

--- react/src/components/Table/ColumnFilter.tsx
+++ react/src/components/Table/ColumnFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Row } from 'react-table';
 import { Flex, Input } from '..';
 import CHROMOSOMES from '../../constants/chromosomes';
