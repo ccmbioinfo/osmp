69132c7332c4177f27d11229bf64d52259a28cc9
Hannah Le <hle@dev-hle.hpc4healthlocal>
2021-10-27 12:52:09 -0400

    Removed duplicate Input code

--- react/src/components/Table/TableFilter/NumberRangeFilter.tsx
+++ react/src/components/Table/TableFilter/NumberRangeFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { Row } from 'react-table';
 import { Column, Input, Typography } from '../..';
 import { FlattenedQueryResponse } from '../Table';
@@ -38,32 +38,18 @@ const NumberRangeFilter: React.FC<NumberRangeFilterProps> = ({
         },
     });
 
-    const [min, max] = useMemo(() => {
-        let min = preFilteredRows.length ? preFilteredRows[0].values[columnId] : 0;
-        let max = preFilteredRows.length ? preFilteredRows[0].values[columnId] : 0;
-        preFilteredRows.forEach(row => {
-            min = Math.min(row.values[columnId], min);
-            max = Math.max(row.values[columnId], max);
-        });
-        return [min, max];
-    }, [columnId, preFilteredRows]);
-
     const handleComparisonValue = (
         columnId: string,
         e: React.ChangeEvent<HTMLInputElement>,
         comparison: ComparisonType
     ) => {
         const val = e.target.value;
-        console.log('this is val', val, isNaN(parseInt(val)));
         if (isNaN(parseInt(val)) && val !== '') {
-            console.log('wrong val', val);
             setError(true);
             setText(val);
         } else {
             setError(false);
-            console.log('empty val', val);
             const num = val === '' ? undefined : parseInt(val, 10);
-            console.log('num', num);
             if (val === '') {
                 setText(val);
             }
@@ -72,73 +58,37 @@ const NumberRangeFilter: React.FC<NumberRangeFilterProps> = ({
             } else if (comparison.greater) {
                 setFilter(columnId, [num, +Infinity]);
             } else {
-                console.log(num);
                 setFilter(columnId, [num, num]);
             }
         }
     };
 
     const filterValue = filters.find(f => f.id === columnId)?.value as Array<number>;
-    console.log('filter value', filterValue);
     const comparison = filterComparison[columnId];
 
-    const resolveComparisonComponent = () => {
-        if (comparison.less) {
-            return (
-                <Input
-                    variant="outlined"
-                    value={filterValue ? filterValue[1] : text}
-                    onChange={e => handleComparisonValue(columnId, e, comparison)}
-                    placeholder={`Max (${max})`}
-                    InputAdornmentStart={
-                        <InputComparisonDropdown
-                            columnId={columnId}
-                            filterComparison={filterComparison}
-                            setFilterComparison={setFilterComparison}
-                            setFilter={setFilter}
-                        />
-                    }
-                />
-            );
-        } else if (comparison.greater) {
-            return (
-                <Input
-                    variant="outlined"
-                    value={filterValue ? filterValue[0] : text}
-                    onChange={e => handleComparisonValue(columnId, e, comparison)}
-                    placeholder={`Min (${min})`}
-                    InputAdornmentStart={
-                        <InputComparisonDropdown
-                            columnId={columnId}
-                            filterComparison={filterComparison}
-                            setFilterComparison={setFilterComparison}
-                            setFilter={setFilter}
-                        />
-                    }
-                />
-            );
-        } else {
-            return (
-                <Input
-                    variant="outlined"
-                    value={filterValue ? filterValue[1] : text}
-                    onChange={e => handleComparisonValue(columnId, e, comparison)}
-                    placeholder="Search"
-                    InputAdornmentStart={
-                        <InputComparisonDropdown
-                            columnId={columnId}
-                            filterComparison={filterComparison}
-                            setFilterComparison={setFilterComparison}
-                            setFilter={setFilter}
-                        />
-                    }
-                />
-            );
+    const resolveComparisonValue = (comparison: ComparisonType) => {
+        if (filterValue) {
+            return comparison.less ? filterValue[1] : filterValue[0];
         }
+        return text;
     };
+
     return (
         <Column>
-            {resolveComparisonComponent()}
+            <Input
+                variant="outlined"
+                value={resolveComparisonValue(comparison)}
+                onChange={e => handleComparisonValue(columnId, e, comparison)}
+                placeholder="Search"
+                InputAdornmentStart={
+                    <InputComparisonDropdown
+                        columnId={columnId}
+                        filterComparison={filterComparison}
+                        setFilterComparison={setFilterComparison}
+                        setFilter={setFilter}
+                    />
+                }
+            />
             {error && <Typography variant="subtitle">Please enter a valid number.</Typography>}
         </Column>
     );
