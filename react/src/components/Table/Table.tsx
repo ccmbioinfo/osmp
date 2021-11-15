696f150687c8d33666ea57663225b0952013ecd0
conor <conorklamann@gmail.com>
2021-11-15 14:54:50 -0500

    memoize filterTypes

--- react/src/components/Table/Table.tsx
+++ react/src/components/Table/Table.tsx
@@ -149,7 +149,22 @@ const Table: React.FC<TableProps> = ({ variantData }) => {
 
     const columnsWithoutFilters = React.useMemo(() => ['contact', 'chromosome'], []);
 
+    const filterTypes = React.useMemo(
+        () => ({
+            multiSelect: (
+                rows: Row<FlattenedQueryResponse>[],
+                columnIds: string[],
+                filterValue: string[]
+            ) =>
+                filterValue.length
+                    ? rows.filter(row => filterValue.includes(row.values[columnIds[0]]))
+                    : rows,
+        }),
+        []
+    );
+
     type Accessor = string | (() => JSX.Element) | ((state: any) => any);
+
     // Dynamically adjust column width based on cell's longest text.
     const getColumnWidth = React.useCallback(
         (data: FlattenedQueryResponse[], accessor: Accessor, headerText: string) => {
@@ -401,12 +416,7 @@ const Table: React.FC<TableProps> = ({ variantData }) => {
             columns,
             defaultColumn,
             data: tableData,
-            filterTypes: {
-                multiSelect: (rows, columnIds, filterValue: string[]) =>
-                    filterValue.length
-                        ? rows.filter(row => filterValue.includes(row.values[columnIds[0]]))
-                        : rows,
-            },
+            filterTypes,
             initialState: {
                 sortBy: sortByArray,
                 hiddenColumns: [
