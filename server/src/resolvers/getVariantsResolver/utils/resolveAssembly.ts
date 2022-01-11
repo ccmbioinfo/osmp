a8d94666ff553a4f1fa049a1a3781155fc659ac1
Hannah Le <hannah.lgbhan@gmail.com>
2022-01-11 12:35:59 -0500

    Simplified typings for resolveAssembly

--- server/src/resolvers/getVariantsResolver/utils/resolveAssembly.ts
+++ server/src/resolvers/getVariantsResolver/utils/resolveAssembly.ts
@@ -1,6 +1,6 @@
 import { AssemblyId } from '../../../types';
 
-const resolveAssembly: (assembly: string) => AssemblyId = assembly =>
-  assembly.includes('38') ? '38' : '37';
+const resolveAssembly = (assembly: AssemblyId) =>
+  assembly.includes('38') ? '38' : ('37' as const);
 
 export default resolveAssembly;
