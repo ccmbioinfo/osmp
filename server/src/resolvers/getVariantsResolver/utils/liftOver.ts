1e5c40941d62ecd9b4d969ce7f62f21ae2f8585e
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2023-07-10 14:16:25 -0400

    Fix "chrchr" mapping in liftOver

--- server/src/resolvers/getVariantsResolver/utils/liftOver.ts
+++ server/src/resolvers/getVariantsResolver/utils/liftOver.ts
@@ -3,6 +3,7 @@ import { tmpdir } from 'os';
 import * as path from 'path';
 import { AssemblyId, VariantQueryDataResult } from '../../../types';
 import { timeitAsync } from '../../../utils/timeit';
+import logger from '../../../logger';
 const exec = require('util').promisify(require('child_process').exec);
 
 const createTmpFile = async () => {
@@ -30,11 +31,23 @@ const liftover = timeitAsync('liftover')(
     dataForLiftover: VariantQueryDataResult[],
     assemblyIdInput: AssemblyId
   ) => {
+    console.debug(
+      `dataForAnnotation: ${dataForAnnotation.length}, dataForLiftover: ${dataForLiftover.length}, assemblyIdInput: ${assemblyIdInput}`
+    );
     // Convert variants from JSON format to BED format.
     // Note that position format is 1-based and BED format is half-open 0-based: https://genome.ucsc.edu/FAQ/FAQformat.html#format1
     const bedstring = dataForLiftover
-      .map(v => `chr${v.variant.chromosome}\t${v.variant.start - 1}\t${v.variant.end}`)
+      .map(
+        v =>
+          `${
+            // if coming from GRCh38, probably starts with chr already
+            v.variant.chromosome.startsWith('chr')
+              ? v.variant.chromosome
+              : 'chr' + v.variant.chromosome
+          }\t${v.variant.start - 1}\t${v.variant.end}`
+      )
       .join('\n');
+    logger.debug(`bedstring: ${bedstring}`);
     const lifted = await createTmpFile();
     const unlifted = await createTmpFile();
     const bedfile = await createTmpFile();
@@ -46,52 +59,66 @@ const liftover = timeitAsync('liftover')(
     } else {
       chain = '/home/node/hg19ToHg38.over.chain';
     }
-    await exec(`liftOver ${bedfile} ${chain} ${lifted} ${unlifted}`);
-    const _liftedVars = await promises.readFile(lifted);
-    const _unliftedVars = await promises.readFile(unlifted);
-    const liftedVars = parseBedStart(_liftedVars.toString());
-    const unliftedVars = parseBedStart(_unliftedVars.toString());
-    const liftedVarsEnd = parseBedEnd(_liftedVars.toString());
+    const liftOverCommand = `liftOver ${bedfile} ${chain} ${lifted} ${unlifted}`;
+    try {
+      const { liftOverStdout, liftOverStderr } = await exec(liftOverCommand);
+      logger.debug(liftOverCommand);
+      logger.debug(`${liftOverStdout}`);
+      logger.debug(`${liftOverStderr}`);
+      const _liftedVars = await promises.readFile(lifted);
+      const _unliftedVars = await promises.readFile(unlifted);
+      const liftedVars = parseBedStart(_liftedVars.toString());
+      const unliftedVars = parseBedStart(_unliftedVars.toString());
+      const liftedVarsEnd = parseBedEnd(_liftedVars.toString());
 
-    const unliftedMap: { [key: string]: boolean } = unliftedVars.reduce(
-      (acc, curr) => ({ ...acc, [curr]: true }),
-      {}
-    );
-    const unliftedVariants: VariantQueryDataResult[] = [];
+      const unliftedMap: { [key: string]: boolean } = unliftedVars.reduce(
+        (acc, curr) => ({ ...acc, [curr]: true }),
+        {}
+      );
+      const unliftedVariants: VariantQueryDataResult[] = [];
 
-    // Merge lifted variants with dataForAnnotation. Filter unmapped variants.
-    dataForLiftover.forEach((v, i) => {
-      if (unliftedMap[(v.variant.start - 1).toString()]) {
-        v.variant.assemblyIdCurrent = v.variant.assemblyId;
-        unliftedVariants.push(v);
-      } else {
-        v.variant.start = Number(liftedVars[i]) + 1; // Convert from BED format to position format.
-        v.variant.end = Number(liftedVarsEnd[i]);
-        v.variant.assemblyIdCurrent = assemblyIdInput;
-        dataForAnnotation.push(v);
-      }
-    });
+      // Merge lifted variants with dataForAnnotation. Filter unmapped variants.
+      dataForLiftover.forEach((v, i) => {
+        if (unliftedMap[(v.variant.start - 1).toString()]) {
+          v.variant.assemblyIdCurrent = v.variant.assemblyId;
+          unliftedVariants.push(v);
+        } else {
+          v.variant.start = Number(liftedVars[i]) + 1; // Convert from BED format to position format.
+          v.variant.end = Number(liftedVarsEnd[i]);
+          v.variant.assemblyIdCurrent = assemblyIdInput;
+          dataForAnnotation.push(v);
+        }
+      });
 
-    // Compute the annotation position for the variants that are in user's requested assembly.
-    let geneStart = Infinity;
-    let geneEnd = 0;
-    dataForAnnotation.forEach(result => {
-      if (result.variant.start < geneStart) {
-        geneStart = result.variant.start;
-      }
-      if (result.variant.end > geneEnd) {
-        geneEnd = result.variant.end;
-      }
-    });
+      // Compute the annotation position for the variants that are in user's requested assembly.
+      let geneStart = Infinity;
+      let geneEnd = 0;
+      dataForAnnotation.forEach(result => {
+        if (result.variant.start < geneStart) {
+          geneStart = result.variant.start;
+        }
+        if (result.variant.end > geneEnd) {
+          geneEnd = result.variant.end;
+        }
+      });
 
-    let annotationPosition = '';
-    if (dataForAnnotation.length > 0)
-      annotationPosition = `${dataForAnnotation[0].variant.chromosome}:${geneStart}-${geneEnd}`;
-    promises.rm(lifted);
-    promises.rm(unlifted);
-    promises.rm(bedfile);
+      let annotationPosition = '';
+      if (dataForAnnotation.length > 0)
+        annotationPosition = `${dataForAnnotation[0].variant.chromosome}:${geneStart}-${geneEnd}`;
+      // promises.rm(lifted);
+      // promises.rm(unlifted);
+      // promises.rm(bedfile);
+      logger.debug('After liftover');
+      logger.debug(`${dataForAnnotation.length}`);
+      logger.debug(`${unliftedVariants.length}`);
+      logger.debug(`Annotation position: '${annotationPosition}'`);
+      logger.debug(JSON.stringify(unliftedVariants[0]));
 
-    return { dataForAnnotation, unliftedVariants, annotationPosition };
+      return { dataForAnnotation, unliftedVariants, annotationPosition };
+    } catch (e) {
+      console.error(e);
+      return { dataForAnnotation, unliftedVariants: [], annotationPosition: '' };
+    }
   }
 );
 
