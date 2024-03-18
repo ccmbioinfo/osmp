044eb5f147618b944b3e507ef80d35df36d51677
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2024-03-18 11:39:44 -0400

    Fix styling

--- react/src/components/Search/GeneCombinedSearch.tsx
+++ react/src/components/Search/GeneCombinedSearch.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
+import { FaCaretDown } from 'react-icons/fa';
 import styled from 'styled-components/macro';
+import { useClickAway } from '../../hooks';
 import { AssemblyId } from '../../types';
-import ComboBox from '../ComboBox';
 import { Flex } from '../Layout';
+import SelectableList, { SelectableListItem, SelectableListWrapper } from '../SelectableList';
 import GeneNameSearch, { GeneSelectionValue } from './GeneNameSearch';
 import GenePositionSearch from './GenePositionSearch';
 
-const Wrapper = styled(Flex)`
+const SearchWrapper = styled(Flex)`
     position: relative
     min-height: 40px;
     flex-wrap: nowrap;
@@ -15,17 +17,77 @@ const Wrapper = styled(Flex)`
     width: 100%;
 `;
 
-interface DropdownProps {
-    options: {
-        id: number,
-        value: string;
-        label: string;
-    }[];
+const DropdownWrapper = styled(Flex)`
+    position: relative;
+    min-height: 38px;
+    flex-grow: 0;
+`;
 
+const DropdownHeader = styled(Flex)`
+    background-color: ${props => props.theme.background.main};
+    border: ${props => props.theme.borders.thin};
+    border-color: ${props => props.theme.colors.muted};
+    border-radius: ${props => props.theme.radii.base};
+    box-shadow: ${props => props.theme.boxShadow};
+    color: ${props => props.theme.colors.muted};
+    padding: 0 ${props => props.theme.space[4]};
+    flex-wrap: nowrap;
+    text-wrap: nowrap;
+    overflow: hidden;
+    white-space: nowrap;
+    min-height: 40px;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+`;
+
+const DropdownHeaderText = styled.div`
+    color: ${props => props.theme.colors.text};
+    cursor: pointer;
+    font-size: 14px;
+`;
+
+interface DropdownProps {
+    options: SelectableListItem<string>[];
+    value: string;
+    onSelect: (option: string) => void;
 }
 
+/**
+ * Dropdown component for selecting search type.
+ */
 const Dropdown: React.FC<DropdownProps> = (props) => {
-    return <></>;
+    // ComboBox styling is too deep, so we need our own dropdown
+
+    const [open, setOpen] = useState<Boolean>(false);
+
+    const ignoreRef = React.useRef() as React.MutableRefObject<HTMLDivElement>;
+    const ref = React.useRef() as React.MutableRefObject<HTMLUListElement>;
+
+    useClickAway(ref, () => setOpen(false), ignoreRef);
+
+    console.log(JSON.stringify(props));
+
+    return <DropdownWrapper ref={ignoreRef}>
+        <DropdownHeader tabIndex={0} role="button" onClick={() => setOpen(true)}>
+            <DropdownHeaderText>{props.value}</DropdownHeaderText>
+            <FaCaretDown style={{
+                marginLeft: "8px"
+            }}/>
+        </DropdownHeader>
+        <SelectableListWrapper>
+            {open && (
+                <SelectableList 
+                ref={ref}
+                options={props.options}
+                onSelect={item => {
+                    props.onSelect(item as string);
+                    setOpen(false);
+                }}
+                />
+            )}
+        </SelectableListWrapper>
+    </DropdownWrapper>;
 };
 
 interface GeneCombinedSearchProps {
@@ -38,6 +100,8 @@ interface GeneCombinedSearchProps {
     onError?: (errorText: string | undefined) => void;
 }
 
+const OPTIONS = ["Gene Name", "Position"];
+
 /**
  * Combined search component for searching by gene name or by position.
  * Contains a dropdown integrated into the search bar where the user can
@@ -45,18 +109,18 @@ interface GeneCombinedSearchProps {
  */
 const GeneCombinedSearch: React.FC<GeneCombinedSearchProps> = (props) => {
 
-    const [searchMode, setSearchMode] = useState("Gene Name");
+    const [searchMode, setSearchMode] = useState<string>(OPTIONS[0]);
+
     return (
-        <Wrapper>
-            <ComboBox 
-            options={["Gene Name", "Region"].map((a, id) => ({
+        <SearchWrapper>
+            <Dropdown 
+            options={OPTIONS.map((a, id) => ({
                 id,
-                label: a,
                 value: a,
+                label: a,
             }))}
-            onSelect={setSearchMode}
+            onSelect={(option) => setSearchMode(option)}
             value={searchMode}
-            placeholder="Select"
             />
             {searchMode === "Gene Name" && 
                 <GeneNameSearch 
@@ -66,14 +130,14 @@ const GeneCombinedSearch: React.FC<GeneCombinedSearchProps> = (props) => {
                 onSelect={props.onNameSelect}
                 />
             }
-            {searchMode === "Region" &&
+            {searchMode === "Position" &&
                 <GenePositionSearch 
                 position={props.genePosition}
                 onChange={props.onPositionChange}
                 onError={props.onError}
                 />
             }
-        </Wrapper>
+        </SearchWrapper>
     );
 };
 
